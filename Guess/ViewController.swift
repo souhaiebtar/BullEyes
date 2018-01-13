b7111292547e0310e64bd7a9832de8d9bbeafef7
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-01-13 17:27:08 +0100

    read value

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,10 +21,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue)";
         let alert  = UIAlertController(title: "Hello, World",
-                                       message: "This is my first app!",
+                                       message: message,
                                        preferredStyle: .alert);
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
         
         alert.addAction(action)
         
@@ -33,7 +34,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func sliderMoved(_ slider: UISlider) {
-        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 
 
