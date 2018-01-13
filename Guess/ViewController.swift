6fd51a4d10384f26895eba91641a72ff684ab6f2
tsouhaieb <tarhounisouhaieb@gmail.com>
2018-01-13 17:03:58 +0100

    after adding different element of the main view

--- Guess/ViewController.swift
+++ Guess/ViewController.swift
@@ -19,6 +19,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func showAlert() {
+        let alert  = UIAlertController(title: "Hello, World",
+                                       message: "This is my first app!",
+                                       preferredStyle: .alert);
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(slider.value)")
+    }
 
 
 }
